01cc7ab9051147b7cbd6562c83dc456c2416e284
Terry Cheng <terrycheng@macbook-pro-2.mynet>
2023-05-19 00:01:23 +0800

    config changes

--- wp-content/themes/breathe-easy/bud.config.js
+++ wp-content/themes/breathe-easy/bud.config.js
@@ -34,7 +34,7 @@ export default async (app) => {
      * Proxy origin (`WP_HOME`)
      * @see {@link https://bud.js.org/docs/bud.proxy/}
      */
-    .proxy('http://example.test')
+    .proxy('http://local.breathe-easy.com')
 
     /**
      * Development origin
