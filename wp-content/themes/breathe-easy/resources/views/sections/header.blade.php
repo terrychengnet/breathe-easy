206d491a52f3e87bd0e747991f6ff27f090c4d83
Terry Cheng <terrycheng@macbook-pro-2.mynet>
2023-05-20 16:47:43 +0800

    init setup

--- wp-content/themes/breathe-easy/resources/views/sections/header.blade.php
+++ wp-content/themes/breathe-easy/resources/views/sections/header.blade.php
@@ -1,11 +1,13 @@
-<header class="banner">
-  <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
-  </a>
+<header class="header">
+  <div class="container mx-auto">
+    <a class="brand" href="{{ home_url('/') }}">
+      <img class="header-logo" src="{{ get_template_directory_uri() }}/resources/images/Header-logo.svg"/>
+    </a>
 
-  @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    </nav>
-  @endif
+    @if (has_nav_menu('primary_navigation'))
+      <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+      </nav>
+    @endif
+  </div>
 </header>
