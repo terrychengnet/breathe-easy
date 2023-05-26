c51d6091641413c0bb2a4ed509cf7693df8842d3
Glass Chan <glasschan@qq.com>
2023-05-26 21:12:43 +0800

    Footer responsive support for tablet and some minor fixes

--- wp-content/themes/breathe-easy/resources/views/sections/footer.blade.php
+++ wp-content/themes/breathe-easy/resources/views/sections/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-blue-second">
-    <div class="footer-wrapper container py-20">
-        <div class="footer-top grid grid-flow-col text-white">
+    <div class="footer-wrapper container py-8 lg:py-20">
+        <div class="footer-top grid grid-cols-3 lg:grid-cols-4 text-white gap-4">
             <div class="footer-widget footer-cols widget-1">
                 <h3>Home</h3>
 
@@ -36,13 +36,16 @@
                 <i class="fa-brands fa-linkedin"></i>
             </div>
 
-            <div class="footer-cols contact-us">
+            <div class="footer-cols contact-us order-first lg:order-last col-span-3 lg:col-span-1 mb-10 lg:mb-0">
                 <div class="contact-us-wrapper bg-blue-main p-8 rounded-2xl grid gap-y-1">
                     <h3 class="mb-8">Contact Us</h3>
-                    <div><i class="fa-solid fa-phone-flip mr-3 w-8"></i><span>+852 9887 7273</span></div>
-                    <div><i class="fa-brands fa-whatsapp mr-3 w-8"></i><span>+852 9887 7273</span></div>
-                    <div class="mt-7 inline-flex items-center"><i class="fa-regular fa-envelope mr-3 w-8"></i><span
-                            class="text-base">info@breathe-easyhk.com</span></div>
+                    <div class="grid grid-rows-2 grid-flow-col lg:grid-rows-1 gap-2 lg:grid-flow-row">
+                        <div><i class="fa-solid fa-phone-flip mr-3 w-8"></i><span>+852 9887 7273</span></div>
+                        <div><i class="fa-brands fa-whatsapp mr-3 w-8"></i><span>+852 9887 7273</span></div>
+                        <div class="lg:mt-7 inline-flex items-center"><i
+                                class="fa-regular fa-envelope mr-3 w-8"></i><span
+                                class="text-2xl lg:text-xl">info@breathe-easyhk.com</span></div>
+                    </div>
                 </div>
             </div>
 
