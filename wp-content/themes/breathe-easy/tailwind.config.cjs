5b85f2481728cffe45d090001408694fadf6e95b
Terry Cheng <terrycheng@macbook-pro-2.mynet>
2023-05-28 17:02:08 +0800

    timeline layout responsive adjustment

--- wp-content/themes/breathe-easy/tailwind.config.cjs
+++ wp-content/themes/breathe-easy/tailwind.config.cjs
@@ -14,6 +14,13 @@ module.exports = {
       },
       container: {
         center: true,
+        // padding: {
+        //   DEFAULT: '1rem',
+        //   sm: '2rem',
+        //   lg: '4rem',
+        //   xl: '5rem',
+        //   '2xl': '6rem',
+        // },
       },
       fontFamily: {
         'roboto': ['Roboto', 'sans-serif']
