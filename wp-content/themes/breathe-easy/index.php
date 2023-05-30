5fd7638b2ca88a0bfa2d64df9b381467bd5e7e0b
Glass Chan <glasschan@qq.com>
2023-05-30 23:40:04 +0800

    acf export added bud.config.js fixed

--- wp-content/themes/breathe-easy/index.php
+++ wp-content/themes/breathe-easy/index.php
@@ -2,34 +2,32 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport"
-        content="width=device-width, initial-scale=1">
+      <meta charset="utf-8">
+      <meta name="viewport"
+            content="width=device-width, initial-scale=1">
 
-  <!-- Font Awesome -->
-  <link href="<?= get_template_directory_uri() ?>/resources/fonts/fontawesome/css/fontawesome.css"
-        rel="stylesheet">
-  <link href="<?= get_template_directory_uri() ?>/resources/fonts/fontawesome/css/brands.css"
-        rel="stylesheet">
-  <link href="<?= get_template_directory_uri() ?>/resources/fonts/fontawesome/css/solid.css"
-        rel="stylesheet">
-  <!-- End Font Awesome -->
+      <!-- Font Awesome -->
+      <link href="<?= get_template_directory_uri() ?>/resources/fonts/fontawesome/css/fontawesome.css"
+            rel="stylesheet">
+      <link href="<?= get_template_directory_uri() ?>/resources/fonts/fontawesome/css/brands.css"
+            rel="stylesheet">
+      <link href="<?= get_template_directory_uri() ?>/resources/fonts/fontawesome/css/solid.css"
+            rel="stylesheet">
+      <!-- End Font Awesome -->
 
-  <?php wp_head(); ?>
-  <script src="https://cdn.master.co/css@beta"></script>
-  <script src="https://cdn.tailwindcss.com"></script>
+      <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-  <?php wp_body_open(); ?>
-  <?php do_action('get_header'); ?>
+      <?php wp_body_open(); ?>
+      <?php do_action('get_header'); ?>
 
-  <div id="app">
-    <?php echo view(app('sage.view'), app('sage.data'))->render(); ?>
-  </div>
+      <div id="app">
+            <?php echo view(app('sage.view'), app('sage.data'))->render(); ?>
+      </div>
 
-  <?php do_action('get_footer'); ?>
-  <?php wp_footer(); ?>
+      <?php do_action('get_footer'); ?>
+      <?php wp_footer(); ?>
 </body>
 
 </html>
