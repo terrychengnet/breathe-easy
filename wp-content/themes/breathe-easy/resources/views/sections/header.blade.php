d537cea2a04a037d39197fb5db2c500c8e60f5c3
glasschan <glasschan@qq.com>
2023-05-31 17:17:40 +0800

    gitignore bud.config.js

--- wp-content/themes/breathe-easy/resources/views/sections/header.blade.php
+++ wp-content/themes/breathe-easy/resources/views/sections/header.blade.php
@@ -1,15 +1,16 @@
-<header class="header fixed z-50 top-0 left-0 right-0 h-[85px] bg-white/20 ease-in duration-100">
-  <div class="container h-[100%] flex justify-between items-center">
-      <a class="header-logo w-[270px] h-[62px] justify-self-start" href="{{ home_url('/') }}">
-        {{-- <img class="header-logo-img " src="{{ get_template_directory_uri() }}/resources/images/Header-logo.svg"/> --}}
-      </a> 
-      @if (has_nav_menu('primary_navigation'))
-        <nav class="nav-primary text-blue-main hidden lg:block" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav flex', 'echo' => false]) !!}
-        </nav>
-      @endif
-      <div class="header-contact">
-        <a class="btn btn-contact" href="/contact-us">Contact Us</a>
-      </div>
-  </div>
+<header class="header fixed z-50 left-0 right-0 h-[85px] bg-white/20 ease-in duration-100">
+    <div class="container h-[100%] flex justify-between items-center">
+        <a class="header-logo w-[270px] h-[62px] justify-self-start" href="{{ home_url('/') }}">
+            {{-- <img class="header-logo-img " src="{{ get_template_directory_uri() }}/resources/images/Header-logo.svg"/> --}}
+        </a>
+        @if (has_nav_menu('primary_navigation'))
+            <nav class="nav-primary text-blue-main hidden lg:block"
+                aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+                {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav flex', 'echo' => false]) !!}
+            </nav>
+        @endif
+        <div class="header-contact">
+            <a class="btn btn-contact" href="/contact-us">Contact Us</a>
+        </div>
+    </div>
 </header>
