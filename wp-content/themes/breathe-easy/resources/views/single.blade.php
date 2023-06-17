99f306f2589c29751e2b94fea69297a099aae59e
glasschan <glasschan@qq.com>
2023-06-18 04:59:11 +0800

    Template build: Blog Archive & Single

--- wp-content/themes/breathe-easy/resources/views/single.blade.php
+++ wp-content/themes/breathe-easy/resources/views/single.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.app')
+@include('partials.page-header')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
-    @includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
-  @endwhile
+    <div class="container mx-auto py-8">
+
+        @while (have_posts())
+            @php
+                the_post();
+            @endphp
+            <article>
+                <div class="content_wrapper container container-narrow my-20">
+                    <div class="mb-9">
+                        <span class="text-black text-2xl">{{ get_the_date() }}</span>
+                    </div>
+                    <div class="content">
+                        {!! get_the_content() !!}
+                    </div>
+                </div>
+            </article>
+        @endwhile
+
+    </div>
 @endsection
